5974e5c7afd030b88c58d3d22cbde9c9870bc749
Dima343316 <72628630+Dima343316@users.noreply.github.com>
2023-09-02 13:12:56 +0300

    Delete tes.py

--- tes.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-
-url = 'https://mos-sud.ru/territorial'  # замените на фактический URL запроса
-params = {'q': 'Москва, Химкинский бульвар, 3, подъезд 3' }  # параметры запроса
-response = requests.get(url, params=params)
-print(response)
